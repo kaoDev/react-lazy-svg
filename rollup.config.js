9f43cbe58b27bfab4887b52102f0ea1f39273f4a
Kalle Ott <kalle.ott@gapless-app.com>
2021-03-26 18:39:51 +0100

    fix: improve compiler settings for esm module don't include tslib helpers for language features available when es modules are supported

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,6 @@
-import typescript from '@rollup/plugin-typescript';
+// @ts-check
+import typescript from 'rollup-plugin-typescript2';
 import filesize from 'rollup-plugin-filesize';
-
-const plugins = [
-  typescript({
-    lib: ['es5', 'es6', 'dom'],
-    target: 'es5',
-    declaration: true,
-    noEmitOnError: true,
-  }),
-  filesize(),
-];
 
 const indexCjs = {
   input: 'src/index.tsx',
@@ -17,23 +8,61 @@ const indexCjs = {
     format: 'cjs',
     file: 'dist/index.cjs.js',
   },
-  plugins,
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          lib: ['es5', 'es6', 'dom'],
+          target: 'es5',
+          declaration: false,
+          noEmitOnError: true,
+        },
+      },
+    }),
+    filesize(),
+  ],
 };
+
 const indexEsm = {
   input: 'src/index.tsx',
   output: {
     format: 'esm',
     file: 'dist/index.esm.js',
   },
-  plugins,
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          lib: ['es5', 'es6', 'dom'],
+          target: 'es2019',
+          declaration: true,
+          noEmitOnError: true,
+        },
+      },
+    }),
+    filesize(),
+  ],
 };
+
 const ssrCjs = {
   input: 'src/ssr.tsx',
   output: {
     format: 'cjs',
     file: 'dist/ssr.js',
   },
-  plugins,
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          lib: ['es5', 'es6', 'dom'],
+          target: 'es5',
+          declaration: true,
+          noEmitOnError: true,
+        },
+      },
+    }),
+    filesize(),
+  ],
 };
 
 export default [indexCjs, indexEsm, ssrCjs];
